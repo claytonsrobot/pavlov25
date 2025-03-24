553e81159ae1597b4b359f9e528a60d3d9e54beb
Clayton Bennett <claytonrobot@gmail.com>
2025-03-24 01:48:26 -0500

    work it

--- tests/__main__.py
+++ tests/__main__.py
@@ -30,50 +30,11 @@ class Test():
         self.add_to_known_instances(key = self.key, classinstance_object = self)
         self.print_description() """
     @staticmethod
-    def do_foo(spare1,spare2):
+    def do_foo(spare1,script=None):
         print("foo")
 
-    def script(in1, in2, in3):
-        # Load the audio file
-        audio_data, sample_rate = audio_utils.load_audio(path_in)
-        
-        # Trim the audio
-        trimmed_audio = audio_utils.trim_audio(audio_data, sample_rate, time_start, time_end)
-
-        # Save the trimmed file
-        audio_utils.save_audio(path_out, trimmed_audio, sample_rate)
-
-        # Confirmation messaging
-        print("Success.")
-
-    def main():
-        #defaults = config_utils.load_defaults('config.toml')
-        defaults = {key:,:,:}
-        parser = argparse.ArgumentParser(description="Trim an audio file.")
-        parser.add_argument("-i1", "--input_file", type=str, default=defaults['in1'], help="spare")
-        parser.add_argument("-i2", "--output_file", type=str, default=defaults['in2'], help="spare")
-        parser.add_argument("-i3", "--start_time", type=float, default=defaults['in3'], help="spare")
-        parser.add_argument("--gui", action="store_true", help="Launch the GUI")
-        
-        args = parser.parse_args()
-        if args.foo:
-            Test.do_foo(vars(args),script=None)
-        else:
-            script(args.input_file, args.output_file, args.start_time, args.end_time)
-
-
-
-class Print:
     @staticmethod
-    def nonprint(line):
-        "nonprint"
-        #print(line)
-        pass
-
-
-
-if False:
-    if __name__ == "__main__":
+    def script(script):
         # " Not like this"
         Print.nonprint("test_object = Test(key)")
         Print.nonprint("or")
@@ -85,12 +46,44 @@ if False:
         elif not(Test.devprint()):
             print(f"Test.devprint() is False.")
 
-elif False:
-    if __name__=='not__main__':
+    def main():
+        #defaults = config_utils.load_defaults('config.toml')
+        defaults = {"devprint":Test.devprint(), "desc":False, "spare":"spare", "foo": False}
+        parser = argparse.ArgumentParser(description="Trim an audio file.")
+        parser.add_argument("--spare", type=str, default=defaults['spare'], help="spare")
+        parser.add_argument("-f","--foo", type=bool, default=defaults['foo'], help="foo")
+        parser.add_argument("-d", "--devprint", type=bool, default=defaults['devprint'], help="Test.devprint()")
+        parser.add_argument("-desc", "--description", type=bool, default=defaults['desc'], help="Test.print_description()")
+        parser.add_argument("-s","--script", action="store_true", help="Run Test.script()")
+        
+        args = parser.parse_args()
+        if args.foo:
+            Test.do_foo(vars(args),script=None)
+        elif args.devprint is not None:
+            Test.devprint()
+        else:
+            Test.script(args.script)
+
+
+
+class Print:
+    @staticmethod
+    def nonprint(line):
+        "nonprint"
+        #print(line)
+        pass
+
+
+if __name__ == "__main__":
+    if False:
+        Test.script()
+            
+
+    elif False:
         app = Test()
         app.onecmd_plus_hooks("test")
         app.reset()
         app.cmdloop()
 
-else:
-    main()
+    else:
+        Test.main()
