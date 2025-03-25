caeba6160202e9f5b7ae74086c44f10df8eedbc3
Clayton Bennett <claytonrobot@gmail.com>
2025-03-25 06:35:17 -0500

    class Test

--- tests/__main__.py
+++ tests/__main__.py
@@ -2,39 +2,50 @@
 Author(s): Clayton Bennett
 Created: 23 March 2025'''
 import argparse
+
+class Print:
+    @staticmethod
+    def nonprint(line):
+        "nonprint"
+        #print(line)
+        pass
+
 class Test():
-    devprint = True # change it here, or addd a config ile upon program maturity in sic month (target: 23 September 2025)
+    global devprint_bool, description
+    devprint_bool = True # change it here, or addd a config ile upon program maturity in sic month (target: 23 September 2025)
+    description = "Test is meant to be called as a library / class: Test.devprint() [bool] \nTest is not designed to be run as an instance, like test_object = Test()."
     known_instances = {}
     
     @classmethod
     def reset(cls):
-        cls.devprint = True
+        devprint_bool = True
         cls.known_instances = {}
     
     @classmethod
     def add_to_known_instances(cls,key,classinstance_object): 
         cls.known_instances.update({key:classinstance_object})
-        
-    def print_description(self):
-        print(f"Do not instantiate the Test class: \n{self.description}")
     
-    @classmethod    
-    def devprint(cls):
+    def print_description():
+        print(f"Do not instantiate the Test class: \n\t{description}")
+    
+       
+    def devprint():
         "This function calls the class value of devprint, to see if the dev comments should be printed"
         "if Test.devprint()" # bool
-        return cls.devprint 
+        return devprint_bool 
     """
     def __init__(self,key): # If this errors, good - the instances are not supposed to be generated.
+        super().__init__()
         self.key = key
         self.description = "Test is meant to be called as a library / class: Test.devprint() [bool] \nTest is not designed to be run as an instance, like test_object = Test()."
         self.add_to_known_instances(key = self.key, classinstance_object = self)
-        self.print_description() """
+        self.print_description()""" 
     @staticmethod
     def do_foo(spare1,script=None):
         print("foo")
 
     @staticmethod
-    def script(script):
+    def script(script=None):
         # " Not like this"
         Print.nonprint("test_object = Test(key)")
         Print.nonprint("or")
@@ -45,45 +56,31 @@ class Test():
             print(f"Test.devprint() = {Test.devprint()}") 
         elif not(Test.devprint()):
             print(f"Test.devprint() is False.")
+        print(script)
 
     def main():
         #defaults = config_utils.load_defaults('config.toml')
         defaults = {"devprint":Test.devprint(), "desc":False, "spare":"spare", "foo": False}
-        parser = argparse.ArgumentParser(description="Trim an audio file.")
+        parser = argparse.ArgumentParser(description="Explore the Test class functionionality.")
         parser.add_argument("--spare", type=str, default=defaults['spare'], help="spare")
         parser.add_argument("-f","--foo", type=bool, default=defaults['foo'], help="foo")
-        parser.add_argument("-d", "--devprint", type=bool, default=defaults['devprint'], help="Test.devprint()")
-        parser.add_argument("-desc", "--description", type=bool, default=defaults['desc'], help="Test.print_description()")
+        parser.add_argument("-d", "--devprint", action="store_true", default=defaults['devprint'], help="Test.devprint()")
+        parser.add_argument("-desc", "--description", action="store_true", default=defaults['desc'], help="Test.print_description()")
         parser.add_argument("-s","--script", action="store_true", help="Run Test.script()")
         
         args = parser.parse_args()
+        print(f"args.script = {args.script}")
         if args.foo:
             Test.do_foo(vars(args),script=None)
+        elif args.description:
+            Test.print_description()
         elif args.devprint is not None:
             Test.devprint()
         else:
-            Test.script(args.script)
-
-
-
-class Print:
-    @staticmethod
-    def nonprint(line):
-        "nonprint"
-        #print(line)
-        pass
-
+            Test.script()
 
 if __name__ == "__main__":
     if False:
-        Test.script()
-            
-
-    elif False:
-        app = Test()
-        app.onecmd_plus_hooks("test")
-        app.reset()
-        app.cmdloop()
-
+        Test.script()            
     else:
         Test.main()
